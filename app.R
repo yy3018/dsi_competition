2fec0100a599d86b1db6cc8914cf23ad8e5d65b1
YilingYang <54852177+yy3018@users.noreply.github.com>
2020-04-21 12:50:36 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -6,18 +6,32 @@ library(nlme)
 
 sampleD <- read_csv("sampleD.csv")[,2:4]
 
+# Random effect output
 LMM <- lme(new ~ days, random = ~ 1+ days | state, data = sampleD)
 rm_effect <- data.frame(random.effects(LMM))
+rm_effect[,'overall'] <- rm_effect[,'X.Intercept.'] + rm_effect[,'days']
 rm_effect[,'bl_rank'] <- order(rm_effect$X.Intercept.,rownames(rm_effect))
 rm_effect[,'time_rank'] <- order(rm_effect$days,rownames(rm_effect))
+rm_effect[,'overall_rank'] <- order(rm_effect$overall,rownames(rm_effect))
 rm_effect[,'state'] <- rownames(rm_effect)
-rm_effect_long <- gather(rm_effect,statistics,value,X.Intercept.:time_rank)
+rm_effect_long <- gather(rm_effect,statistics,value,X.Intercept.:overall_rank)
+rm_effect_long[rm_effect_long$statistics=='X.Intercept.','statistics'] = 'baseline'
 
 
-#fix_effect <- data.frame(fixed.effects(LMM))
+# Fixed effect output
+fix_effect <- data.frame(fixed.effects(LMM))
+fix_effect[,'statistics'] <- c('baseline','days')
+colnames(fix_effect)[1] <- 'value' 
 
+stay_home_time <- read_csv("stay_home_time.csv")
 
-order(rm_effect$X.Intercept.,rownames(rm_effect))
+table1 <- stay_home_time
+table1[,'baseline'] <- fix_effect$value[1]
+table1[,'days'] <- fix_effect$value[2]
+
+# Other summary
+
+avg_total <- sampleD %>% group_by(state) %>% summarise('AVG' = mean(new),'Total'=sum(new))
 
 u <- fluidPage(
   
@@ -88,7 +102,9 @@ s <- function(input, output) {
   # Display statistical output
   
   output$tb1 <- renderTable({
-    rm_effect_long[rm_effect_long$state == input$states,]
+    
+    t1 <- table1[table1$state == input$states,]
+    gather(left_join(t1,avg_total,by='state'),statistics,value,time:Total)
   })
   
   output$tb2 <- renderTable({
