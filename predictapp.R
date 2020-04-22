3773772efef5a54ba7d8d663fd24f5b00b68abef
lyyleah7 <yl4360@cumc.columbia.edu>
2020-04-22 19:11:20 -0400

    parachange

--- predictapp.R
+++ predictapp.R
@@ -2,9 +2,7 @@ library(shiny)
 library(tidyverse)
 library(EpiDynamics)
 
-# read data
-death_rate = read_csv("./data/death_rate.csv") %>% janitor::clean_names() 
-state = death_rate$location
+
 # shiny app
 ui = fluidPage(
   
@@ -18,27 +16,12 @@ ui = fluidPage(
     sidebarPanel(
       helpText("Create a SEIR Model"),
       
-      selectInput("state", 
-                  label = "Choose a state",
-                  choices = state ,
-                  selected = "New York"),
+      sliderInput("time", h6("Time/days"),
+                  min = 0, max = 60, value = 200),
       
-      # Input: Slider for the number of bins ----
       numericInput("beta", 
                    h6("Beta"), 
                    value = 2.47),
-      
-      numericInput("sigma", 
-                   h6("Sigma"), 
-                   value = 0.192),
-      
-      numericInput("gamma", 
-                   h6("Gamma"), 
-                   value = 0.434),
-      
-      numericInput("mu", 
-                   h6("Mu"), 
-                   value = 0.0005),
       
       numericInput("s", 
                    h6("Susceptible"), 
@@ -50,14 +33,7 @@ ui = fluidPage(
       
       numericInput("i", 
                    h6("Infectious"), 
-                   value = 0),
-      
-      numericInput("r", 
-                   h6("Removed/Recovered"), 
-                   value = 0),
-      
-      sliderInput("time", h6("Time/days"),
-                  min = 0, max = 300, value = 60)
+                   value = 0)
       
     ),
     
@@ -73,8 +49,13 @@ ui = fluidPage(
 
 server = function(input, output) {
   output$curve = renderPlot({
-    parameters = list(beta = input$beta, sigma = input$sigma, gamma = input$gamma, mu=input$mu)
-    initials = c(S=input$s, E=input$e, I=input$i, R=input$r)
+
+    parameters = list(beta = input$beta, sigma = 0.192, gamma = 0.434, mu=0.0005)
+    S=input$s
+    E=input$e
+    I=input$i
+    R=1-S-E-I
+    initials = c(S=S,E=E,I=I,R=R)
     seir = SEIR(pars = parameters,init = initials, time = 0:input$time) # time unit is day
     PlotMods(seir)
   })
