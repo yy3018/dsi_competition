035afcc5d08857c60af0d20a569d5f9f40d49e5e
lyyleah7 <yl4360@cumc.columbia.edu>
2020-04-22 19:55:50 -0400

    app

--- predictapp.R
+++ predictapp.R
@@ -1,7 +1,8 @@
 library(shiny)
 library(tidyverse)
 library(EpiDynamics)
-
+library(deSolve)
+library(plotly)
 
 # shiny app
 ui = fluidPage(
@@ -25,11 +26,11 @@ ui = fluidPage(
       
       numericInput("s", 
                    h6("Susceptible"), 
-                   value = 0.9),
+                   value = 0.9999),
       
       numericInput("e", 
                    h6("Exposed"), 
-                   value = 0.1),
+                   value = 0.0001),
       
       numericInput("i", 
                    h6("Infectious"), 
@@ -41,23 +42,56 @@ ui = fluidPage(
     mainPanel(
       
       # Output: Histogram ----
-      plotOutput("curve")
+      plotlyOutput("curve")
       
     )
   )
 )
 
 server = function(input, output) {
-  output$curve = renderPlot({
-
-    parameters = list(beta = input$beta, sigma = 0.192, gamma = 0.434, mu=0.0005)
+  output$curve = renderPlotly({
+    
+    function2 <- function(time, init, pars) {
+      with(as.list(c(init, pars)), {
+        dS = mu - S * (beta * I + mu)
+        dE = beta * S * I - E * (sigma + mu)
+        dI = sigma * E - I * (gamma + mu)
+        dR = gamma * I - mu * R
+        list(c(dS, dE, dI, dR))
+      })
+    }
+    
+    parameters = list(beta = input$beta, sigma = 0.192, gamma = 0.434, mu=0.00005)
     S=input$s
     E=input$e
     I=input$i
     R=1-S-E-I
     initials = c(S=S,E=E,I=I,R=R)
-    seir = SEIR(pars = parameters,init = initials, time = 0:input$time) # time unit is day
-    PlotMods(seir)
+    
+    output = ode(times = 0:input$time, func = function2, y = initials, parms = parameters)
+    
+    visual_data = as.data.frame(output)
+    
+    vis_seir = visual_data %>% 
+      mutate(
+        p_S = S,
+        p_E = E,
+        p_I = I,
+        p_R = R
+      ) %>% 
+      select(time,p_S, p_E, p_I, p_R) %>% 
+      pivot_longer(
+        p_S:p_R,
+        names_to = "group", 
+        names_prefix = "p_",
+        values_to = "proportion")
+    
+    fig2 = vis_seir %>% 
+      ggplot(aes(x = time, y = proportion,group = group,color = group)) + 
+      geom_line()+
+      theme_bw()
+    
+    ggplotly(fig2)
   })
 }
 
