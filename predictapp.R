b968bcf229fc9758ce35276067a6eacb60c80525
lyyleah7 <yl4360@cumc.columbia.edu>
2020-04-23 21:46:41 -0400

    prediction final version

--- predictapp.R
+++ predictapp.R
@@ -8,21 +8,20 @@ library(plotly)
 ui = fluidPage(
   theme = "united",
   # App title ----
-  titlePanel("SEIR Model: What does "),
+  titlePanel(h2("Social Distancing Effect - Create an SEIR Model Yourself!")),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
     
     # Sidebar panel for inputs ----
     sidebarPanel(
-      helpText("Create a SEIR Model"),
       
       sliderInput("time", h6("Time/days"),
-                  min = 0, max = 200, value = 100),
+                  min = 0, max = 300, value = 200),
       fluidRow(
       column(6,numericInput("beta", 
                    h6("Beta"), 
-                   value = 2.47)),
+                   value = 0.741)),
       
       column(6,numericInput("s", 
                    h6("Susceptible"), 
@@ -37,8 +36,12 @@ ui = fluidPage(
                    value = 0))),
       
       sliderInput("beta_change", 
-                   h6("Social Distance Strength"), 
-                   min = 0, max = 500, value = 50)
+                   h6("Decrease in Beta (i.e. with social distancing)"), 
+                   min = 0, max = 1, value = 0.48),
+      sliderInput("capacity", 
+                  h6("Healthcare System Capacity"), 
+                  min = 0, max = 0.06, value = 0.05)
+      
       
     ),
     
@@ -46,12 +49,22 @@ ui = fluidPage(
     mainPanel(
       fluidRow(
       column(7, plotlyOutput("curve1", width = "100%", height = "250px")),
-      column(5, textOutput("text1"))
+      column(5, h4(strong("Parameters explanation:")),
+                 div(strong("Beta:"), "the average contact rate"),
+                 div(strong("Sigma:"), "the inverse of the incubation period (set as 0.156)", tags$a(href="https://www.ncbi.nlm.nih.gov/pubmed/32046819", "[1]") ),
+                 div(strong("Gamma:"), " the inverse of the mean infectious period (set as 0.153)",tags$a(href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7139011/", "[2]")),
+                 div(strong("Mu:"), " the per capita death rate regardless of decease (set as 0.007)"),
+                 div(strong("SEIR:"), "susceptible, exposed, infected and recoverd/removed."),
+                div(strong("A/B:"),"after/before social distancing."))
       ),
       
       fluidRow(
         column(7, plotlyOutput("curve2", width = "100%", height = "250px")),
-        column(5, textOutput("text2"))
+        column(5, h4(strong("Model Results:")),
+                  div("When more social distancing takes place through the population (i.e. beta decreases), 
+                      we can see the flattening effect. For instance, when beta decreases 0.48 from 0.741, 
+                      the peak of A-curve meets the healthcare system capacity which is considered to be capable to cover 5% of population.
+                      From this result, we reveal the effect of social distancing."))
       )
       
     )
@@ -71,7 +84,7 @@ server = function(input, output) {
       })
     }
     
-    parameters = list(beta = input$beta, sigma = 0.192, gamma = 0.434, mu=0.00005)
+    parameters = list(beta = input$beta, sigma =  0.156, gamma = 0.153, mu=0.007)
     S=input$s
     E=input$e
     I=input$i
@@ -99,7 +112,11 @@ server = function(input, output) {
     fig2 = vis_seir %>% 
       ggplot(aes(x = time, y = proportion,group = group,color = group)) + 
       geom_line()+
-      theme_bw()
+      theme_bw()+
+      labs(x = "Time(days)", y="Population Fraction", title = "Fig.1 COVID-19 SEIR Model")+
+      theme(plot.title = element_text(size=10, hjust = 0.5),
+            axis.title = element_text(size = 7),
+            legend.title = element_blank())
     
     ggplotly(fig2)
   })
@@ -115,15 +132,15 @@ server = function(input, output) {
       })
     }
     
-    parameters = list(beta = input$beta, sigma = 0.192, gamma = 0.434, mu=0.00005)
+    parameters = list(beta = input$beta, sigma =  0.156, gamma = 0.153, mu=0.007)
     S=input$s
     E=input$e
     I=input$i
     R=1-S-E-I
     initials = c(S=S,E=E,I=I,R=R)
-    parameters.2 = list(beta = input$beta-(input$beta_change)/100,
-                        sigma = 0.192, gamma = 0.434,
-                        mu = 0.00005
+    parameters.2 = list(beta = input$beta-input$beta_change,
+                        sigma = 0.156, gamma = 0.153,
+                        mu = 0.007
     )
     
     output = ode(times = 0:input$time, func = function2, y = initials, parms = parameters)
@@ -142,15 +159,17 @@ server = function(input, output) {
       ggplot(aes(x = time, y = I,color = group)) + 
       geom_line()+
       theme_bw()+
-      labs(y="Infectious proportion")+
-      geom_hline(yintercept=0.05, color = "red", linetype="dashed", size=1)+
-      annotate("text", x=input$time*2/3, y=0.057, label="Healthcare system capacity", size = 3, color = "red")
+      labs(x = "Time(days)", y="Infected Population Proportion", title = "Fig.2 COVID-19 SEIR Model with Social Distancing")+
+      geom_hline(yintercept=input$capacity, color = "red", linetype="dashed", size=1)+
+      annotate("text", x=input$time*2/3, y=input$capacity+0.007, label="Healthcare system capacity", size = 3, color = "red")+
+      theme(plot.title = element_text(size=10, hjust = 0.5),
+            axis.title = element_text(size = 7),
+            legend.title = element_blank())
+    
     ggplotly(fig1)
   })
   
-  output$text1 = renderText("Model explanation")
-  
-  output$text2 = renderText("social distance")
+
 }
 
 shinyApp(ui, server)
